d76494e29de0f7ca3d33ad56e159fe567ae52d19
YamidCampo <69608801+YamidCampo@users.noreply.github.com>
2025-02-17 14:01:10 -0500

    intento

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,11 +2,13 @@
 """
 Escriba el codigo que ejecute la accion solicitada.
 """
-
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
 
 def pregunta_01():
     """
-    El archivo `files//shipping-data.csv` contiene información sobre los envios
+    El archivo `files//shipping-copia.csv` contiene información sobre los envios
     de productos de una empresa. Cree un dashboard estático en HTML que
     permita visualizar los siguientes campos:
 
@@ -28,10 +30,92 @@ def pregunta_01():
 
     Tenga en cuenta los siguientes cambios respecto al video:
 
-    * El archivo de datos se encuentra en la carpeta `data`.
+    * El archivo de datos se encuentra en la carpeta `copia`.
 
     * Todos los archivos debe ser creados en la carpeta `docs`.
 
     * Su código debe crear la carpeta `docs` si no existe.
 
     """
+
+    # Creación carpeta "docs"
+    os.makedirs("docs", exist_ok=True)
+
+    # Lectura datos y creacíon base datos
+    df=pd.read_csv("files/input/shipping-data.csv")
+
+    copia = df.copy()
+
+    # Gráfio warehouse
+    plt.figure()
+    counts=copia["Warehouse_block"].value_counts()
+    counts.plot.bar(
+        title='Shipping per Warehouse',
+        xlabel='Warehouse block',
+        ylabel='Record Count',
+        color='tab:blue',
+        fontsize=8,
+    )
+
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.savefig("docs/shipping_per_warehouse.png")
+
+
+    # Gráfico Mode of Shipment
+    plt.figure()
+    counts=copia["Mode_of_Shipment"].value_counts()
+    counts.plot.pie(
+        title='Mode of Shipment',
+        wedgeprops=dict(width=0.35),
+        ylabel="",
+        colors=['tab:blue','tab:orange','tab:green'],
+    )
+
+    plt.savefig("docs/mode_of_shipment.png")
+
+    # Grafica customer rating
+    plt.figure()
+    copia = (copia[["Mode_of_Shipment", "Customer_rating"]]
+            .groupby("Mode_of_Shipment").describe())
+    copia.columns = copia.columns.droplevel()
+    copia = copia[["mean","min","max"]]
+    plt.barh(
+        y=copia.index.values,
+        width=copia["max"].values -1,
+        left=copia["min"].values,
+        height=0.9,
+        color="lightgray",
+        alpha=0.5
+    )
+    colors=["tab:green" if value>=3.0 else "tab:orange" for value in copia["mean"].values]
+    plt.barh(
+        y=copia.index.values,
+        width=copia["max"].values - 1,
+        left=copia["min"].values,
+        height=0.5,
+        color=colors,
+        alpha=1.0
+    )
+    plt.title("Average customer rating")
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.gca().spines["left"].set_visible(False)
+    plt.gca().spines["bottom"].set_visible(False)
+
+    plt.savefig("docs/average_customer_rating.png")
+
+
+    # Grafica weight distribution
+    copia2 = df.copy()
+    plt.figure()
+    copia2.Weight_in_gms.plot.hist(
+        title="Shipping weight distribution",
+        color="tab:orange",
+        edgecolor="white",
+    )
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.savefig("docs/weight_distribution.png")
+
+pregunta_01()
